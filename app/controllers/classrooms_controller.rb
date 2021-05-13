6d4be2ce0f431d63bdd7ef7080c533e0442e6a2c
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-05-13 12:47:29 -0400

    links student and notes

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,2 +1,23 @@
 class ClassroomsController < ApplicationController
+
+    def index 
+        @student_id = session[:student_id]
+        @classrooms = Classroom.all
+        # binding.pry
+    end
+
+   
+    def show 
+        @classroom = Classroom.find_by(id: params[:id])
+    end
+
+
+
+    private 
+
+    def classroom_params
+    end
+
+
+    
 end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,12 +1,48 @@
 class NotesController < ApplicationController
 
     def new
-        binding.pry
+        @note = Note.new 
     end
+
+
 
 
     def create 
+        
+       
+        @note = Note.new(notes_params)
+        @note.student = current_student
+        binding.pry
+        if @note.save 
+            binding.pry
+            redirect_to note_path(@note)
+        else
+            render :new
+        end  
     end
+
+    def show 
+        @note = Note.find_by(id: params[:id])
+    end
+
+
+
+
+    def edit 
+        @note = Note.find_by(id: params[:id])
+    end
+
+    def update 
+        @note = Note.find_by(id: params[:id])
+        if @note.update(notes_params)
+            redirect_to note_path(@note)
+        else
+            render :edit
+        end
+    end
+
+
+
 
     private 
 
